a727960d5c06ea9b0b32948310c79ef22385b087
Sumato Ken <bermime1@gmail.com>
2023-03-02 21:08:43 +0100

    refactored code

--- demo.py
+++ demo.py
@@ -1,55 +1,23 @@
-import os
-import azure.cognitiveservices.speech as speechsdk
-from speechSynthetis import textToSpeech
-from wake import recognise_wake_word
-from engine import generatePrompt, generateResponse
-
-speech_config = speechsdk.SpeechConfig(
-    subscription=os.environ.get("AZURE_SPEECH_KEY"),
-    region=os.environ.get("AZURE_SPEECH_REGION"),
-)
-speech_config.speech_recognition_language = "en-US"
-audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
-audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
-speech_recognizer = speechsdk.SpeechRecognizer(
-    speech_config=speech_config, audio_config=audio_config
+from engine import (
+    generatePrompt,
+    generateResponse,
+    listenToSpeech,
+    listenForWakeWord,
+    textToSpeech,
+    addToContext,
 )
 
 
-initiated = False
+def run():
+    recognisedWakeWord = listenForWakeWord()
+    while recognisedWakeWord:
+        recognisedSpeech = listenToSpeech()
+        if recognisedSpeech == "exit.":
+            exit()
+        prompt = generatePrompt(recognisedSpeech)
+        response = generateResponse(prompt)
+        addToContext(recognisedSpeech, response)
+        textToSpeech(response)
 
 
-def run(initiated):
-    recognised_wake_word = recognise_wake_word()
-    print(recognised_wake_word)
-    while recognised_wake_word:
-        if initiated != True:
-            print("How may I help you?")
-            textToSpeech(
-                "How may I help you?",
-                speech_config=speech_config,
-                audio_config=audio_config,
-            )
-        initiated = True
-        speech_recognition_result = speech_recognizer.recognize_once_async().get()
-
-        if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
-            prompt = generatePrompt(speech_recognition_result.text)
-            response = generateResponse(prompt)
-            textToSpeech(
-                response,
-                speech_config=speech_config,
-                audio_config=audio_config,
-            )
-        elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
-            print(
-                "No speech could be recognized: {}".format(
-                    speech_recognition_result.no_match_details
-                )
-            )
-        elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
-            cancellation_details = speech_recognition_result.cancellation_details
-            print("Speech Recognition canceled: {}".format(cancellation_details))
-
-
-run(initiated)
+run()
--- engine.py
+++ engine.py
@@ -1,44 +1,129 @@
 from prompt_engine.chat_engine import ChatEngine, ChatEngineConfig
 from prompt_engine.model_config import ModelConfig
 from prompt_engine.interaction import Interaction
+import os
+import azure.cognitiveservices.speech as speechsdk
+from dotenv import load_dotenv
 import openai
 import os
 
+openai.organization = "org-xwIudUz9vZhKbTbBroTcMtfP"
+openai.api_key = os.environ.get("OPENAI_KEY")
+
+speech_config = speechsdk.SpeechConfig(
+    subscription=os.environ.get("AZURE_SPEECH_KEY"),
+    region=os.environ.get("AZURE_SPEECH_REGION"),
+)
+speech_config.speech_recognition_language = "en-US"
+audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
+speechRecognizer = speechsdk.SpeechRecognizer(
+    speech_config=speech_config, audio_config=audio_config
+)
+
+config = ChatEngineConfig(ModelConfig(max_tokens=1024), bot_name="ECHO")
+description = "Your name is Echo and you're capable of holding conversations with humans. You always make your answer in one and a half sentence. When you don't have an answer, only reply with (I don't know) or any other variation. When the prompt is unclear always respond with (Please say again) or any other variation."
+examples = [
+    Interaction("Hello", "Hey, how are you?"),
+    Interaction("I am feeling okay, and you?", "I'm in my best shape!"),
+    Interaction(
+        "What is a quantum state?",
+        "In quantum mechanics, a quantum state is a precise mathematical description of a physical system that includes both position and momentum.",
+    ),
+    Interaction(
+        "What is Compton effect?",
+        "The Compton effect, first described by Arthur Holly Compton, is the phenomenon in which an x-ray or gamma ray photon interacts with an electron.",
+    ),
+    Interaction(
+        "How can I generate signals exploiting the properties of operational amplifiers?",
+        "Operational amplifiers (or op amps) can be used to generate various types of signals",
+    ),
+]
+global chat_engine
+chat_engine = ChatEngine(config=config, description=description, examples=examples)
+
+
+def listenForWakeWord():
+    model = speechsdk.KeywordRecognitionModel("keyword.table")
+    keyword = "Hey Echo"
+    wakeWordRecognizer = speechsdk.KeywordRecognizer()
+
+    def recognized_cb(evt):
+        result = evt.result
+        if result.reason == speechsdk.ResultReason.RecognizedKeyword:
+            print("RECOGNIZED KEYWORD: {}".format(result.text))
+        return True
+
+    def canceled_cb(evt):
+        result = evt.result
+        if result.reason == speechsdk.ResultReason.Canceled:
+            print("CANCELED: {}".format(result.cancellation_details.reason))
+            return False
+
+    wakeWordRecognizer.recognized.connect(recognized_cb)
+    wakeWordRecognizer.canceled.connect(canceled_cb)
+    recognisedWakeWord = wakeWordRecognizer.recognize_once_async(model)
+    print(
+        'Say something starting with "{}" followed by whatever you want...'.format(
+            keyword
+        )
+    )
+    result = recognisedWakeWord.get()
+
+    if result.reason == speechsdk.ResultReason.RecognizedKeyword:
+        stop_future = wakeWordRecognizer.stop_recognition_async()
+        print("Stopping...")
+        stopped = stop_future.get()
+        return True
+
+
+def textToSpeech(text):
+    speech_config.speech_synthesis_voice_name = "en-US-JennyNeural"
+    speech_synthesizer = speechsdk.SpeechSynthesizer(
+        speech_config=speech_config, audio_config=audio_config
+    )
+    speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
+    if (
+        speech_synthesis_result.reason
+        == speechsdk.ResultReason.SynthesizingAudioCompleted
+    ):
+        return True
+    elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = speech_synthesis_result.cancellation_details
+        print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+
 
 def generatePrompt(query):
-    config = ChatEngineConfig(ModelConfig(max_tokens=1024))
-    description = "You are a robot capable of holding conversations with intellectual humans. You always make your answer as short as possible"
-    examples = [
-        Interaction("Hello", "Hey, how are you?"),
-        Interaction("I am feeling okay, and you?", "I'm in my best shape!"),
-        Interaction(
-            "How many characters are in an average two sentences?",
-            "Depending on the length of the sentences, the exact number of characters can vary. However, an average two sentences would usually be around 60-80 characters.",
-        ),
-        Interaction(
-            "What is a quantum state?",
-            "In quantum mechanics, a quantum state is a precise mathematical description of a physical system that includes both position and momentum.",
-        ),
-        Interaction(
-            "What is Compton effect?",
-            "The Compton effect, first described by Arthur Holly Compton, is the phenomenon in which an x-ray or gamma ray photon interacts with an electron, resulting in the photon's energy being partially transferred to the electron.",
-        ),
-        Interaction(
-            "How can I generate signals exploiting the properties of operational amplifiers?",
-            "Operational amplifiers (or op amps) can be used to generate various types of signals. For example, you can use the op amp to generate a sine wave by combining a non-inverting and inverting amplifier, or you can use the op amp to generate a square wave by using hysteresis.",
-        ),
-    ]
-    chat_engine = ChatEngine(config=config, description=description, examples=examples)
     return chat_engine.build_prompt(query)
 
 
+def addToContext(query, response):
+    chat_engine.add_interaction(query, response)
+
+
 def generateResponse(prompt):
-    openai.organization = "org-xwIudUz9vZhKbTbBroTcMtfP"
-    openai.api_key = os.environ.get("OPENAI_KEY")
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
         max_tokens=60,
         temperature=0.7,
     )
-    return response.choices[0].text
+    return response.choices[0].text.split("ECHO: ")[1]
+
+
+def listenToSpeech():
+    print("Listening")
+    recognisedSpeech = speechRecognizer.recognize_once_async().get()
+    if recognisedSpeech.reason == speechsdk.ResultReason.RecognizedSpeech:
+        return recognisedSpeech.text
+    elif recognisedSpeech.reason == speechsdk.ResultReason.NoMatch:
+        print(
+            "No speech could be recognized: {}".format(
+                recognisedSpeech.no_match_details
+            )
+        )
+        return "No speech could be recognized:"
+    elif recognisedSpeech.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = recognisedSpeech.cancellation_details
+        print("Speech Recognition canceled: {}".format(cancellation_details))
+        return "Error"
